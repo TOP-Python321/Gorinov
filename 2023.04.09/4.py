e412a1ebc389de415496d56e29099f7e1b1c8b13
Gennadiy <renardes@gmail.com>
2023-04-11 16:52:24 +0500

    2023.04.09: комментарии, правки, итоги

--- 2023.04.09/4.py
+++ 2023.04.09/4.py
@@ -1,10 +1,23 @@
-#num_var = int(input('Введите трехзначное число: '))
+# num_var = int(input('Введите трехзначное число: '))
 num_var = int(input())
-unit_var = num_var%10
-dozen_var = (int(num_var/10))%10
-hundred_var = int(num_var/100)
+
+# КОММЕНТАРИЙ: единицы (разряд числа) — ones, единица измерения — unit
+unit_var = num_var % 10
+# КОММЕНТАРИЙ: десятки (разряд числа) — tens, (примерно) десяток, дюжина — dozen
+# ИСПРАВИТЬ: скобки вокруг int() не нужны
+dozen_var = (int(num_var / 10)) % 10
+hundred_var = int(num_var / 100)
+
 sum_var = hundred_var + dozen_var + unit_var
 pro_var = hundred_var * dozen_var * unit_var
-print(f'Сумма цифр = {sum_var} \nПроизведение цифр = {pro_var}')
-#Сумма цифр = 12
-#Произведение цифр = 64
+
+# ИСПОЛЬЗОВАТЬ: перенос строковых литералов внутри скобок
+print(f'Сумма цифр = {sum_var} \n'
+      f'Произведение цифр = {pro_var}')
+
+
+# Сумма цифр = 12
+# Произведение цифр = 64
+
+
+# ИТОГ: отлично — 4/4
