04c99ba3cd31770a954a1fc6ec25eddee96e8d33
Andrejgas <andrejgas1@gmail.com>
2023-04-14 19:58:15 +0300

    доработка первого варианта

--- 2023.04.09/5.py
+++ 2023.04.09/5.py
@@ -2,7 +2,8 @@ int_var = int(input())
 # КОММЕНТАРИЙ: decimal — десятичный, float — плавающий
 float_var = int(input())
 
-miles_var = float(f'{int_var}.{float_var}')
+miles_var = int_var + float_var / 10**len(str(float_var))
+# miles_var = float(f'{int_var}.{float_var}')
 km_var = round(miles_var * 1.61, 1)
 
 print(f'{miles_var} миль = {km_var} км')
