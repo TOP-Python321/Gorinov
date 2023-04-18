d3b65db5321776560d155ea57be97c21494e5682
Andrejgas <andrejgas1@gmail.com>
2023-04-18 19:22:02 +0300

    доработка

--- 2023.04.16/1.py
+++ 2023.04.16/1.py
@@ -2,22 +2,22 @@ inp_1 = float(input('Введите первое число: '))
 inp_2 = float(input('Введите второе число: ')) 
 inp_3 = float(input('Введите третье число: '))
 
-if inp_1 < 0:
-    inp_1 = 0
+sum_inp = 0
 
-if inp_2 < 0:
-    inp_2 = 0
+if inp_1 > 0:
+    sum_inp += inp_1 
 
-if inp_3 < 0:
-    inp_3 = 0
+if inp_2 > 0:
+    sum_inp += inp_2
+
+if inp_3 > 0:
+    sum_inp += inp_3
     
-sum_inp = inp_1 + inp_2 + inp_3
-
 if sum_inp - int(sum_inp) > 0:
     print(round(sum_inp, 2))
     
 else:
     print(round(sum_inp))
     
-# 4.57
+# 5.5
     
