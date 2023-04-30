debf7884f8770e8bfaa917482a7a21d43695ed1b
Горинов Андрей <andrejgas1@gmail.com>
2023-05-01 01:11:02 +0300

    замена цикла на генератор

--- 2023.04.23/6.py
+++ 2023.04.23/6.py
@@ -1,18 +1,28 @@
 num = input('Введите шестизначное число: ')
 
-sum_num_1, sum_num_2 = 0, 0
+sum_num_1 = sum(int(num[i]) for i in range(3))
+sum_num_2 = sum(int(num[i]) for i in range(3,6))
 
-for i in range(3):
-    sum_num_1 += int(num[i])
-    
-for i in range(3, 6):
-    sum_num_2 += int(num[i])
-    
 if sum_num_1 == sum_num_2:
     print('Да')
     
 else:
     print('Нет')
+
+# решение через цикл    
+# sum_num_1, sum_num_2 = 0, 0
+
+# for i in range(3):
+    # sum_num_1 += int(num[i])
+    
+# for i in range(3, 6):
+    # sum_num_2 += int(num[i])
+# if sum_num_1 == sum_num_2:
+    # print('Да')
+    
+# else:
+    # print('Нет')   
+
 
 # Введите шестизначное число: 123321
 
