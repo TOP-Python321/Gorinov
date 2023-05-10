251f6959211af6a2b0b3846e6af175f29abd3327
Andrejgas <andrejgas1@gmail.com>
2023-05-11 00:08:15 +0300

    del startswith('1b')

--- 2023.04.30/6.py
+++ 2023.04.30/6.py
@@ -5,8 +5,8 @@ binary_set = {'0', '1', 'b'}
 
 if (not (set_inp <= binary_set)
    or inp.count('b') > 1 
-   or inp.startswith('1b')
-   # or 'b' in inp and '1' in inp[:inp.index('b')]
+   # or inp.startswith('1b')
+   or 'b' in inp and '1' in inp[:inp.index('b')]
    # эти проверки возможно лишние
    or 'b' in inp and len(inp) == 1
    or '0b' in inp and len(inp) == 2
