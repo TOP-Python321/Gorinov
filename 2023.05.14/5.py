07cd19ed0b0c73e8ad69cc5d2d82243ab31430c8
Andrejgas <andrejgas1@gmail.com>
2023-05-21 19:04:11 +0300

    add files

---
+++ 2023.05.14/5.py
@@ -0,0 +1,33 @@
+def central_tendency(arg_1: float, arg_2: float, /, *numbers: float) -> dict[str, float]:
+
+    """Возвращает словарь, в котором: ключами являются меры центральной тенденции, а значения содержат результат вычислений """
+
+    list_numbers = [arg_1, arg_2]
+    list_numbers.extend(list(numbers))
+    list_numbers.sort()    
+    dict_out = {}
+    
+    if len(list_numbers) % 2:
+        dict_out |= dict(median = float(list_numbers[len(list_numbers) // 2]))
+    else:
+        dict_out |= dict(median = (list_numbers[len(list_numbers) // 2] + list_numbers[len(list_numbers) // 2 - 1]) / 2)
+        
+    dict_out |= dict(arithmetic = sum(list_numbers) / len(list_numbers))
+    
+     
+    multi = 1
+    for num in list_numbers:      
+        multi *= num
+        
+    dict_out |= dict(geometric = multi ** (1 / len(list_numbers)))
+
+
+    dict_out |= dict(harmonic = len(list_numbers) / sum(1 / i for i in list_numbers))
+    
+    return dict_out
+    
+# >>> central_tendency(1,3, 4, 2)
+# {'median': 2.5, 'arithmetic': 2.5, 'geometric': 2.213363839400643, 'harmonic': 1.9200000000000004}
+# >>> central_tendency(1,3, 4, 2, 15, 120)
+# {'median': 3.5, 'arithmetic': 24.166666666666668, 'geometric': 5.923530438729457, 'harmonic': 2.77992277992278}
+
---
+++ 2023.05.14/6.py
@@ -0,0 +1,20 @@
+def orth_triangle(*,cathetus1: float = 0,cathetus2: float = 0, hypotenuse: float = 0) -> float | None:
+
+    """Возвращает результат вычисления третьей стороны треугольника, если вычисление не возможно, возвращает None"""
+
+    if not cathetus1 and cathetus2 and cathetus2 < hypotenuse:
+        return float((hypotenuse **2 - cathetus2 **2) ** (1 / 2))
+        
+    elif not cathetus2 and cathetus1 and cathetus1 < hypotenuse:
+        return float((hypotenuse **2 - cathetus1 **2) ** (1 / 2))
+        
+    elif not hypotenuse and cathetus1 and cathetus2:
+        return float((cathetus1 ** 2 + cathetus2 ** 2) ** (1 / 2))
+        
+        
+# >>> orth_triangle(cathetus1=6, cathetus2=8)
+# 10.0
+# >>> orth_triangle(cathetus1=6, hypotenuse=10)
+# 8.0
+# >>> print(orth_triangle(cathetus2=8,cathetus1=4, hypotenuse=10))
+# None
