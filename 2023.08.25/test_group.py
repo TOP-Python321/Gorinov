1c34c0d9ff1dac64c9b784913013abbdd13017ec
Andrejgas <andrejgas1@gmail.com>
2023-08-25 22:42:13 +0300

    добавление аннотаций test_group.py

--- 2023.08.25/test_group.py
+++ 2023.08.25/test_group.py
@@ -1,38 +1,65 @@
 from random import randrange as rr, choice, sample
 from string import digits, ascii_uppercase, ascii_lowercase
-from datetime import date, datetime as dt, timedelta
+from datetime import date, timedelta
 
 import test
 
-def produce_group(teachers: list[test.Teacher, ...], students: list[test.Student, ...], semester: int = 1, qty: int = rr(12, 25)):
+
+def produce_group(
+        teachers: list[test.Teacher, ...],
+        students: list[test.Student, ...],
+        semester: int = 1,
+        qty: int = rr(12, 25)
+        ) -> test.Group:
+    """
+    Создает и возвращает объект класса Group.
+    :param teachers: список из объектов Teacher
+    :param students: список из объектов Student
+    :param semester: номер семестра, по умолчанию - 1
+    :param qty: количество студентов в группе
+    :return: объект класса Group
+    """
     group = test.Group(semester, choice(teachers), *sample(students, k=qty))
     return group
-    
-def mobile() -> str:        
+
+
+def mobile() -> str:
+    """Генерирует номер мобильного телефона."""
     mob_num = ''.join(sample(digits, k=9))
     return f'+79{mob_num}'
-    
+
+
 def office() -> str:
+    """Генерирует номер стационарного телефона."""
     return ''.join(sample(digits, k=9))
-    
+
+
 def gen_str():
-    return ''.join(sample(ascii_lowercase, k=rr(6,10)))
-   
+    """Генерирует строку из произвольных символов."""
+    return ''.join(sample(ascii_lowercase, k=rr(6, 10)))
+
+
 def title() -> str:
-    return choice(ascii_uppercase) + ''.join(sample(ascii_lowercase, k=rr(6,10)))
-      
+    """Генерирует названии организации."""
+    return choice(ascii_uppercase) + ''.join(sample(ascii_lowercase, k=rr(6, 10)))
+
+
 def email() -> str:
+    """Генерирует электронный адрес"""
     return f"{''.join(sample(ascii_lowercase, k=rr(6,10)))}@{choice(['mail.ru', 'gmail.com'])}" 
 
-def gen_dt():
+
+def gen_dt() -> date:
+    """Генерирует объект date"""
     _dt = date.today() + timedelta(rr(2, 90))
     return _dt
 
-def t_delta():
+
+def t_delta() -> timedelta:
+    """Генерирует объект timedelta"""
     return timedelta(rr(2, 8))
-    
-def gen_con():
+
+
+def gen_con() -> test.Contact:
+    """Генерирует и возвращает объект test.Contact."""
     return test.Contact(mobile(), office(), email())
-
-
-    
